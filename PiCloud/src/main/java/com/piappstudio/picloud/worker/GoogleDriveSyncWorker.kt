ed10dd7657383f2203c28a7651db16470c073081
Pi App Studio <70383910+piappstudio@users.noreply.github.com>
2022-11-02 02:01:47 -0500

    Draft

--- PiCloud/src/main/java/com/piappstudio/picloud/worker/GoogleDriveSyncWorker.kt
+++ PiCloud/src/main/java/com/piappstudio/picloud/worker/GoogleDriveSyncWorker.kt
@@ -10,7 +10,6 @@ import android.content.Context
 import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
-import com.piappstudio.picloud.R
 import com.piappstudio.pimodel.Constant
 import com.piappstudio.pimodel.Resource
 import com.piappstudio.pimodel.pref.PiPrefKey
@@ -21,6 +20,8 @@ import dagger.hilt.EntryPoint
 import dagger.hilt.InstallIn
 import dagger.hilt.android.EntryPointAccessors
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.util.*
 
 @HiltWorker
@@ -41,17 +42,14 @@ class GoogleDriveSyncWorker @AssistedInject constructor(
             EntryPointAccessors.fromApplication(context, WorkerProviderEntryPoint::class.java)
         val piDriveManager = entryPoint.piDriveManager()
         val piPreference = entryPoint.piPreference()
-        val title = context.getString(R.string.drive_sync_notification_title)
         piDriveManager.doSync().collect {
-            //makeStatusNotification(title, it.data ?: context.getString(R.string.default_message), context)
+            it.data?.let { progress ->
+                setForeground(createForegroundInfo(progress))
+            }
             if (it.status == Resource.Status.SUCCESS) {
                 val date = Constant.PiFormat.orderItemDisplay.format(Date())
                 piPreference.save(PiPrefKey.LAST_SYNC_TIME, date)
-                makeStatusNotification(
-                    title,
-                    message = "Last sync date: $date",
-                    context = context
-                )
+                setForeground(createForegroundInfo("Last sync date: $date"))
             }
         }
         return Result.success()
